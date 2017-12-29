99b27ae11df24f8ad8f7b0d5938411da2a550754
Yeondam Park <giyen9088@gmail.com>
2017-12-28 23:42:15 -0800

    add actions, action creators, reducer functions

--- redux/modules/user.js
+++ redux/modules/user.js
@@ -1,9 +1,34 @@
 // Imports
 import { API_URL } from "../../constants";
+import { AsyncStorage } from "react-native";
 
 // Actions
 
+const LOG_IN = "LOG_IN";
+const LOG_OUT = "LOG_OUT";
+const SET_USER = "SET_USER";
+
 // Action Creators
+
+function setLogIn(token) {
+  return {
+    type: LOG_IN,
+    token
+  };
+}
+
+function setLogOut() {
+  return {
+    type: LOG_OUT
+  };
+}
+
+function setUser(user) {
+  return {
+    type: SET_USER,
+    user
+  };
+}
 
 // API Actions
 
@@ -20,7 +45,14 @@ function login(username, password) {
       })
     })
       .then(response => response.json())
-      .then(json => console.log(json));
+      .then(json => {
+        if (json.token) {
+          dispatch(setLogIn(json.token));
+        }
+        if (json.user) {
+          dispatch(setUser(json.user));
+        }
+      });
   };
 }
 
@@ -33,12 +65,44 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case LOG_IN:
+      return applyLogIn(state, action);
+    case LOG_OUT:
+      return applyLogOut(state, action);
+    case SET_USER:
+      return applySetUser(state, action);
     default:
       return state;
   }
 }
 
 // Reducer Functions
+
+function applyLogIn(state, action) {
+  const { token } = action;
+  return {
+    ...state,
+    isLoggedIn: true,
+    token
+  };
+}
+
+function applyLogOut(state, action) {
+  AsyncStorage.clear();
+  return {
+    ...state,
+    isLoggedIn: false,
+    token: ""
+  };
+}
+
+function applySetUser(state, action) {
+  const { user } = action;
+  return {
+    ...state,
+    profile: user
+  };
+}
 
 // Exports
 
