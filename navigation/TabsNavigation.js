89c280915e6f4a9ac8e5ff8966de6059532baab5
Yeondam Park <giyen9088@gmail.com>
2018-01-01 21:33:58 -0800

    stack navigations added

--- navigation/TabsNavigation.js
+++ navigation/TabsNavigation.js
@@ -1,11 +1,35 @@
 import React from "react";
 import { TabNavigator, TabBarBottom } from "react-navigation";
+import HomeRoute from "../routes/HomeRoute";
+import SearchRoute from "../routes/SearchRoute";
+import NotificationsRoute from "../routes/NotificationsRoute";
+import ProfileRoute from "../routes/ProfileRoute";
 import { Ionicons } from "@expo/vector-icons";
 
 const TabsNavigation = TabNavigator(
-  {},
   {
-    tabBarPosition: "bottom"
+    Home: {
+      screen: HomeRoute
+    },
+    Search: {
+      screen: SearchRoute
+    },
+    Notifications: {
+      screen: NotificationsRoute
+    },
+    Profile: {
+      screen: ProfileRoute
+    }
+  },
+  {
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      showLabel: false,
+      style: {
+        backgroundColor: "#FBFBFB",
+        height: 45
+      }
+    }
   }
 );
 
