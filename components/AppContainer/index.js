308b95cac49f659bfc75faaaf8e1bfae4b9dcac4
Yeondam Park <giyen9088@gmail.com>
2018-01-03 00:19:25 -0800

    creating initiApp and getFeed functions

--- components/AppContainer/index.js
+++ components/AppContainer/index.js
@@ -1,11 +1,20 @@
 import { connect } from "react-redux";
 import AppContainer from "./presenter";
+import { actionCreators as photoActions } from "../../redux/modules/photo";
 
 const mapStateToProps = (state, ownProps) => {
   const { user } = state;
   return {
     isLoggedIn: user.isLoggedIn,
     profile: user.profile
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    initApp: () => {
+      dispatch(photoActions.getFeed());
+    }
   };
 };
 
