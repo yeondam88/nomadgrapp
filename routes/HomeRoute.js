2cd815882b74ae2ff84ed7b472cc595b0c9997e7
Yeondam Park <giyen9088@gmail.com>
2018-01-02 23:49:45 -0800

    creating the Feed header

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,11 +1,29 @@
+import React from "react";
 import { StackNavigator } from "react-navigation";
+import { Image } from "react-native";
+import NavButton from "../components/NavButton";
 import FeedScreen from "../screens/FeedScreen";
 import sharedRoutes, { sharedOptions } from "./sharedRoutes";
 
 const HomeRoute = StackNavigator(
   {
     Home: {
-      screen: FeedScreen
+      screen: FeedScreen,
+      navigationOptions: ({ navigation }) => ({
+        headerTitle: (
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={{ height: 35 }}
+            resizeMode={"contain"}
+          />
+        ),
+        headerLeft: (
+          <NavButton
+            iconName={"ios-camera-outline"}
+            onPress={() => navigation.navigate("TakePhoto")}
+          />
+        )
+      })
     },
     ...sharedRoutes
   },
