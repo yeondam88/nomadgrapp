e408a3d8ac963aeb4d0cf5a6e7980c98548dd74f
Yeondam Park <giyen9088@gmail.com>
2018-01-16 21:35:01 -0800

    more screens

---
+++ screens/PhotoScreen/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+const PhotoScreen = props => <Text>PHoto Screen</Text>;
+
+export default PhotoScreen;
---
+++ screens/ProfileDetailScreen/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+const ProfileDetailScreen = props => <Text>Profile Detail Screen</Text>;
+
+export default ProfileDetailScreen;
