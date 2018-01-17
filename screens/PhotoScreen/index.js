7be4ece68197c559523f215e22737c4027d09f3e
Yeondam Park <giyen9088@gmail.com>
2018-01-16 22:10:01 -0800

    Profile component part 1

--- screens/PhotoScreen/index.js
+++ screens/PhotoScreen/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text } from "react-native";
 
-const PhotoScreen = props => <Text>PHoto Screen</Text>;
+const PhotoScreen = props => <Text>Photo Screen</Text>;
 
 export default PhotoScreen;
