282bcc7d86cfa2deb8ea7ebe47802ab61c31f5c1
Yeondam Park <giyen9088@gmail.com>
2018-01-23 00:00:31 -0800

    add camera screen

---
+++ screens/CameraScreen/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const CameraScreen = props => <Text>Upload Photo</Text>;
+
+export default CameraScreen;
---
+++ screens/LibraryScreen/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LibraryScreen = props => <Text>Library Photo</Text>;
+
+export default LibraryScreen;
