becaad5c5a4fe40370c3770c6a54737cc874d2db
Yeondam Park <giyen9088@gmail.com>
2018-01-24 22:58:57 -0800

    library screen finished

--- screens/LibraryScreen/presenter.js
+++ screens/LibraryScreen/presenter.js
@@ -1,6 +1,60 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import FitImage from "react-native-fit-image";
 
-const LibraryScreen = props => <Text>Library Photo</Text>;
+const { height, width } = Dimensions.get("window");
+
+const LibraryScreen = props => (
+  <View style={styles.container}>
+    {props.photos && (
+      <View style={styles.pictureContainer}>
+        <FitImage source={{ uri: props.pickedPhoto.node.image.uri }} />
+      </View>
+    )}
+    {props.photos && (
+      <View style={styles.photos}>
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+          {props.photos.map((photo, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => props.pickPhoto(photo)}
+            >
+              <Image
+                source={{ uri: photo.node.image.uri }}
+                style={styles.smallPhoto}
+              />
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    )}
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  pictureContainer: {
+    flex: 2,
+    justifyContent: "center"
+  },
+  scrollViewContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap"
+  },
+  smallPhoto: {
+    width: width / 3,
+    height: width / 3
+  }
+});
 
 export default LibraryScreen;
